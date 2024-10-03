84be8d9c8b1acf131e6f2a2631c4baa3b0f240f6
jsoltan226 <jsoltan226@github.com>
2024-10-03 17:41:02 +0200

    Added sleep functions in platform/time

--- platform/linux/time.c
+++ platform/linux/time.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #define _POSIX_C_SOURCE 199309L
 #include "../time.h"
 #include <core/int.h>
@@ -10,7 +11,9 @@
 #define MODULE_NAME "time"
 
 /* clangd is really stupid ngl */
-int clock_gettime (__clockid_t __clock_id, struct timespec *__tp);
+extern int clock_gettime (__clockid_t __clock_id, struct timespec *__tp);
+extern int clock_nanosleep(__clockid_t __clock_id, int flags,
+    const struct timespec *rqtp, struct timespec *rmtp);
 
 i32 p_time(p_time_t *o)
 {
@@ -48,4 +51,29 @@ i32 p_time_since(p_time_t *o, p_time_t *since)
     o->s -= since->s;
 
     return 0;
+}
+
+void p_time_nanosleep(p_time_t *time)
+{
+    u64 ns_sum = time->ns + (time->us * 1000) + (time->ms * 1000000);
+
+    /* Handle potential overflows */
+    u64 s_sum = time->s + (ns_sum / 1000000000);
+    ns_sum = ns_sum % 1000000000;
+
+    struct timespec ts_req = {
+        .tv_sec = s_sum,
+        .tv_nsec = ns_sum,
+    };
+    clock_nanosleep(CLOCK_REALTIME, 0, &ts_req, NULL);
+}
+
+void p_time_usleep(u32 u_seconds)
+{
+    p_time_nanosleep(&(p_time_t) { .us = u_seconds });
+}
+
+void p_time_sleep(u32 seconds)
+{
+    p_time_nanosleep(&(p_time_t) { .s = seconds });
 }
--- platform/time.h
+++ platform/time.h
@@ -17,4 +17,8 @@ struct p_timer {
 i32 p_time_since(p_time_t *o, p_time_t *since);
 i32 p_time(p_time_t *o);
 
+void p_time_nanosleep(p_time_t *time);
+void p_time_usleep(u32 u_seconds);
+void p_time_sleep(u32 seconds);
+
 #endif /* P_TIME_H_ */
